76c74e280c86ec866ea38882304bcec5dc355aba
soulchicken <soulfever01@naver.com>
2022-06-13 01:58:06 +0900

    FEAT : profile save 기능 - But 아직 확인못함
    - 데스크탑에 mysql을 다시 받아야하는 상황이 되어버림!
    - 일단 될 것 같은데 확인이 필요함.

--- App/src/main/java/com/mylittlelib/app/controller/ProfileController.java
+++ App/src/main/java/com/mylittlelib/app/controller/ProfileController.java
@@ -1,4 +1,23 @@
 package com.mylittlelib.app.controller;
 
+import com.mylittlelib.app.DTO.ProfileDTO;
+import com.mylittlelib.app.model.Profile;
+import com.mylittlelib.app.service.ProfileService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("profile")
 public class ProfileController {
+    @Autowired
+    private ProfileService profileService;
+
+    @PostMapping
+    public ProfileDTO save(@RequestBody Profile profile){
+        return profileService.save(profile);
+    }
+
 }
--- App/src/main/java/com/mylittlelib/app/service/ProfileService.java
+++ App/src/main/java/com/mylittlelib/app/service/ProfileService.java
@@ -1,12 +1,43 @@
 package com.mylittlelib.app.service;
 
+import com.mylittlelib.app.DTO.ProfileDTO;
+import com.mylittlelib.app.model.Profile;
+import com.mylittlelib.app.repository.ProfileRepository;
 import com.mylittlelib.app.repository.UserRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
+@Slf4j
 public class ProfileService {
 
+
     @Autowired
-    private UserRepository userRepository;
+    public ProfileRepository profileRepository;
+
+    @Autowired
+    public UserRepository userRepository;
+
+    public ProfileDTO save(Profile profile) {
+        if (! userRepository.existsUserByUserId(profile.getUser().getUserId())){
+            log.error("해당 유저가 존재하지 않습니다.");
+            throw new RuntimeException("[ERROR] 해당 유저가 존재하지 않습니다.");
+        }
+        if (profileRepository.existsByUser(profile.getUser())){
+            log.error("이미 프로필이 존재합니다.");
+            throw new RuntimeException("[ERROR] 이미 프로필이 존재합니다.");
+        }
+        profileRepository.save(profile);
+        ProfileDTO profileDTO = ProfileDTO
+                .builder()
+                .profileIndex(profile.getProfileIndex())
+                .imageUrl(profile.getImageUrl())
+                .userIndex(profile.getUser().getUserIndex())
+                .build();
+
+        return profileDTO;
+    }
+
+
 }
