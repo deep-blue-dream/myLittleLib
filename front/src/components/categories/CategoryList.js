bca7826ebaffadd82a8056681c30f5bfb2b32ecb
tutleCord <102152693+deep-blue-dream@users.noreply.github.com>
2022-06-14 17:58:47 +0900

    feat : GoogleSignUpAPI 추가 we are the champion!

--- front/src/components/categories/CategoryList.js
+++ front/src/components/categories/CategoryList.js
@@ -5,7 +5,7 @@ import Category from './Category/Category'
 const CategoryList = (props) => {
     // console.log(props); 값 넘어오는 것은 확인
     const categories = props.categorydata.map(category => (
-        <Category key={category}
+        <Category key={category.id}
         id={category.id}
         category = {category.category}
         title={category.title}
--- front/src/modal/AddCategoryModal.js
+++ front/src/modal/AddCategoryModal.js
@@ -54,15 +54,15 @@ const AddCategoryModal = ({ setModalOn}) => {
                     <h3 className="mb-4 text-xl font-medium text-gray-900 dark:text-white">나만의 북마크 폴더 등록</h3>
                         <form className="space-y-3" action="#">
                             {/* <div>
-                                <label htmlfor="category" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">주제</label>
+                                <label htmlFor="category" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">주제</label>
                                 <input type="text" name="category" id="category" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" placeholder="음악/유튜브" required=""/>
                             </div> */}
                             <div>
-                                <label htmlfor="title" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">제목</label>
+                                <label htmlFor="title" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">제목</label>
                                 <input type="text" name="title" id="title" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" onChange={categoryTitleChangeHandler} placeholder="멜로디 전용" required=""/>
                             </div>
                             <div>
-                                <label htmlfor="description" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">설명</label>
+                                <label htmlFor="description" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">설명</label>
                                 <input type="text" name="description" id="description" placeholder="공부할 때 듣기 좋은 음악 리스트" className="box-border w-72 h-40 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" onChange={categoryDesChangeHandler} required=""/>
                             </div>
                                 <div className="flex justify-between">
