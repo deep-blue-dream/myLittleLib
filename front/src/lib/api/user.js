948fa35b751312ee5760f7bbee6347f904d1bbee
gun-0208 <67572011+gun-0208@users.noreply.github.com>
2022-06-15 12:01:14 +0900

    feat : 로그인/로그아웃 및 세션 유지
    @wonillin

--- front/src/lib/api/user.js
+++ front/src/lib/api/user.js
@@ -1,13 +1,23 @@
 import baseURL from '.';
 
 //POST 카테고리 등록 API
-export const postSignInAPI = (data) => fetch(`${baseURL}/user/signin`, {
-    method: 'POST',
-    headers: {
-        'Content-Type': 'application/json'
-    },
-    body: JSON.stringify(data)
-});
+export const postSignInAPI = async (data) => {
+
+    const res = await fetch(`${baseURL}/user/signin`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+    });
+    const user =  await res.json();
+
+    // user.then(result => result);
+
+    // console.log(user);
+    return user;
+
+}
 
 
 
